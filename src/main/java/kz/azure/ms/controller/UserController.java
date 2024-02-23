e828b1e4e9138e5b5f1da51ee2f64ce6595f2f18
DumanBruh <dmarlambekov@gmail.com>
2024-02-23 21:45:58 +0600

    #1 Authorization logic added

--- src/main/java/kz/azure/ms/controller/UserController.java
+++ src/main/java/kz/azure/ms/controller/UserController.java
@@ -2,10 +2,10 @@ package kz.azure.ms.controller;
 
 import kz.azure.ms.model.dto.UserLoginRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+
+import java.util.Base64;
 
 @RestController
 @RequestMapping(value = "auth")
@@ -22,14 +24,19 @@ public class UserController {
 
 
     @PostMapping("/login")
-    public Mono<ResponseEntity> login(@RequestBody UserLoginRequest request) {
+    public Mono<ResponseEntity<?>> login(@RequestBody UserLoginRequest request) {
         return userDetailsService.findByUsername(request.getUsername())
-                .map(UserDetails::getPassword)
-                .map(storedHashedPassword -> {
+                .map(userDetails -> {
                     PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-                    boolean matches = passwordEncoder.matches(request.getPassword(), storedHashedPassword);
-                    return matches ? new ResponseEntity<>(HttpStatus.OK) : new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-                });
+                    if (passwordEncoder.matches(request.getPassword(), userDetails.getPassword())) {
+                        String authValue = "Basic " + Base64.getEncoder().encodeToString((request.getUsername() + ":" + request.getPassword()).getBytes());
+                        HttpHeaders headers = new HttpHeaders();
+                        headers.add(HttpHeaders.AUTHORIZATION, authValue);
+                        return ResponseEntity.ok().headers(headers).body("Login Successful");
+                    } else {
+                        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+                    }
+                }).defaultIfEmpty(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
     }
 
     @PostMapping("/register")
